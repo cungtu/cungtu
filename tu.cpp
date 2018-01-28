65c47d5280f08e4e1a9aa766b809eb8433fdee45
cungtu <ceeter1999@gmail.com>
2018-01-28 16:19:01 +0700

    tu.cpp

--- tu.cpp
+++ tu.cpp
@@ -1,1 +1,17 @@
-# cungtu
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int a,b,s=0 ;
+	cin >>a;
+	cin >>b;
+	
+	for (int i=a;i<=b;i++)
+	{
+		s+=i;
+	}
+cout<<s<<endl;
+return 0;
+}
+# hw4-3-1-cungtu
